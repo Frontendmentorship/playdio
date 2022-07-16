11862078f82428f1c6b5e1d13219eed3975a0901
Daniel Don <dandons25@gmail.com>
2022-07-16 19:04:14 +0100

    create input example and adjust eslint

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import OnboardInput from '../../components/OnboardInput';
 
 function Home() {
-    return <div>Home Page</div>;
+  return (
+    <div>
+      Home Page
+      <OnboardInput label="Password" />
+    </div>
+  );
 }
 
 export default Home;
