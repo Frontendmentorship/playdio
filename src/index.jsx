9918f5b52fff05513901b239470039982e61ffcc
Gideon Idoko <iamgideonidoko@gmail.com>
2022-08-05 20:48:46 +0100

    Disable stricmode for music player

--- src/index.jsx
+++ src/index.jsx
@@ -6,12 +6,18 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <Router>
+//       <App />
+//     </Router>
+//   </React.StrictMode>,
+// );
+
 root.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
+  <Router>
+    <App />
+  </Router>,
 );
 
 // If you want to start measuring performance in your app, pass a function
